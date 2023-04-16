c0b514abc96988ba0e47adf48ed23673e87e3a5c
Dayane Ramos <dayanecode@gmail.com>
2023-04-16 11:33:35 -0300

    contempla o carregamento de dados

---
+++ src/hooks/useFetchDocuments.js
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import { 
+            collection, 
+            query, 
+            orderBy, 
+            onSnapshot, 
+            where,
+            QuerySnapshot,
+         } from "firebase/firestore";
+import { async } from "q";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+
+    const [documents, setDocuments] = useState(null)
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    // deal with memory leak
+    const [cancelled, setCancelled] = useState(false)
+
+    useEffect(() => {
+
+        async function loadData() {
+            if(cancelled) return
+
+            setLoading(true)
+
+            const collectionRef = await collection(db, docCollection)
+
+            try {
+
+                let q
+
+                // busca
+
+                // dashboard
+                
+                q = await query (collectionRef, orderBy("createAt", "desc"));
+
+                // Vai servir para mapear os nosso dados
+                await onSnapshot(q, (querySnapshot) => {
+                    setDocuments(
+                        querySnapshot.docs.map((doc) => ({
+                            id: doc.id,
+                            ...doc.data(),
+                        }))
+                    );
+                });
+
+                setLoading(false);
+            } catch (error) {
+                console.log(error)
+                setError(error.message);
+
+                setLoading(false);
+            }
+        }
+
+        loadData();
+
+    }, [docCollection, search, uid, cancelled]);
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, []);
+
+    return { documents, loading, error };
+};
+
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,13 +4,16 @@ import styles from "./Home.module.css"
 // hooks
 import { useNavigate, Link } from "react-router-dom"
 import { useState } from "react"
+import { useFetchDocuments } from "../../hooks/useFetchDocuments"
+import PostDetail from "../../components/PostDetail"
+
 
 // components
 
 const Home = () => {
 
   const [query, setQuery] = useState("")
-  const [posts] = useState([])
+  const { documents: posts, loading } = useFetchDocuments("posts"); 
   
 
   const handleSubmit = (e) => {
@@ -25,11 +28,15 @@ const Home = () => {
         <button className="btn btn-dark">Pesquisar</button>
         </form>
         <div>
-          <h1>Posts...</h1>
+          {loading && <p>Carregando...</p>}
+          {/* {posts && posts.map((post) =>  <h3>{post.title}</h3> )} */}
+          {posts && posts.map((post) => <PostDetail post={post} /> )}
           {posts && posts.length === 0 && (
             <div className={styles.noposts}>
               <p>Não foram encontrados posts</p>
-              <Link to="/posts/create" className="btn" >Criar o primeiro post</Link>
+              <Link to="/posts/create" className="btn">
+                Criar o primeiro post
+              </Link>
             </div>
           )}
         </div>
