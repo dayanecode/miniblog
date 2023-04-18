1b856d3b84ddc928a94758e67a3106258c1382da
Dayane Ramos <dayanecode@gmail.com>
2023-04-18 13:43:39 -0300

    testar a home sem carregar

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom'
 const PostDetail = ({post}) => {
 
   // conversão do creatAt para formato de data e de hora
-  const createAt = post.createAt
-  const date = createAt.toDate();
-  const formattedDate = date.toLocaleDateString();
-  const formattedTime = date.toLocaleTimeString();
+  // const createAt = post.createAt
+  // const date = createAt.toDate();
+  // const formattedDate = date.toLocaleDateString();
+  // const formattedTime = date.toLocaleTimeString();
 
   return (
     <div className={styles.post_detail}>
         <img src={post.image} alt={post.title} />
-      <p className={styles.createby}>Criado em: {formattedDate} {formattedTime} - Por: {post.createBy}</p>
+      {/* <p className={styles.createby}>Criado em: {formattedDate} {formattedTime} - Por: {post.createBy}</p> */}
         <h2>{post.title}</h2>
         <div className={styles.tags}>
             {post.tagsArray.map((tag) => (
