1df22d88fe46002a94db2dbe1615a6e815d440eb
Dayane Ramos <dayanecode@gmail.com>
2023-04-18 18:40:45 -0300

    iniciado o css da dashboard e incluida class noposts nos componentes globais

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -14,10 +14,17 @@ const Dashboard = () => {
 
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
+  const deleteDocument = (id) => {}
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
-    <div> 
+    <div className={styles.dashboard}> 
         <h2>Dashboard</h2>
         <p>Gerencie os seus posts</p>
+        {/* Quando não tiver pos */}
         {posts && posts.length === 0 ? (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
@@ -26,11 +33,30 @@ const Dashboard = () => {
             </Link>
           </div>
         ) : (
-          <div> 
-            <p>Tem posts!</p>
-          </div>
+          <>
+            <div className={styles.post_header}> 
+             <span>Título</span>
+             <span>Ações</span>
+            </div>
+            {/* renderizar linha onde exibe o post com os botões de ação */}
+            {posts &&
+              posts.map((post) => (
+              <div className={styles.post_row} key={post.id}>
+               <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
+                <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">Editar</Link>
+                <button 
+                    onClick={() => deleteDocument(post.id)}
+                    className="btn btn-outline btn-danger"
+                >
+                  Excluir
+                </button>
+              </div>
+            </div>
+            ))}
+          </>
         )}
-      {posts && posts.map((post) => <PostDetail key={post.id} post={post} /> )}
     </div>
   )
 }
