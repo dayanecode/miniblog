22e904d60befd967ef58b84d6c76b91d66d65ecf
dayanecode <dayanecode@gmail.com>
2023-05-24 09:30:20 -0300

    alter name component Avatar for AvatarCollection

---
+++ src/components/Avatar/AvatarCollection.js
@@ -0,0 +1,47 @@
+import styles from './AvatarCollection.module.css'
+
+import { useState, useEffect } from 'react'
+
+const AvatarCollection = (props) => {
+
+    const [posts, setPosts] = useState([]);
+  
+    useEffect(() => fetchPosts(), []);
+  
+    function fetchPosts() {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.json())
+        .then((data) => {
+          data.lenght = 10;
+          setPosts(data); //passa os dados de uma API para o meu setPosts
+        })
+        .catch((err) => console.log(err));
+    }
+      
+    // Método que captura a URL do Avatar
+    const handleDoubleClick = (e) => {
+        const url = e.target.src
+        // Envia a URL para o componente Register(pai)
+        window.parent.postMessage(url, '*')
+        props.handleClick(url);   
+    }
+
+  return (
+    <div>
+      <div className={styles.avatar_collection}>
+              {posts.map((post) => (     
+            <div className={styles.avatar}>
+              <img
+                src={`https://avatars.dicebear.com/api/avataaars/${post.id}.svg`} 
+                alt='Avatar do autor'
+                className='author-avatar'
+                onDoubleClick={handleDoubleClick}
+              />
+            </div>    
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default AvatarCollection
