14d9bd0462945089733e8d05d22ad1b0c6cb754a
dayanecode <dayanecode@gmail.com>
2023-06-05 22:48:42 -0300

    improved responsiveness

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -27,12 +27,14 @@ const Home = () => {
 
   return (
     <div className={styles.home}>
-        <h1>Veja os nossos posts mais recentes</h1>
-        <form onSubmit={handleSubmit} className={styles.search_form}>
-        <input type="text" placeholder="Ou busque por tags..." onChange={(e) => setQuery(e.target.value)} />
-        <button className="btn btn-dark">Pesquisar</button>
-        </form>
-        <div>
+      <div className={styles.search_container}>
+          <h1>Veja os nossos posts mais recentes</h1>
+          <form onSubmit={handleSubmit} className={styles.search_form}>
+          <input type="text" placeholder="Ou busque por tags..." onChange={(e) => setQuery(e.target.value)} />
+          <button className="btn btn-dark">Pesquisar</button>
+          </form>
+        </div>
+        <div className={styles.post_detail}>
           {loading && <p>Carregando...</p>}
           {posts && posts.length === 0 && (
             <div className={styles.noposts}>
